a3ee8cdd7786c8456de75c2740254be86ddc0e41
Jim <jim~@126.com>
2022-03-17 15:48:03 +0800

    add: `/views/pages/index.html`

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
-// refer: https://www.expressModulejs.com.cn/starter/hello-world.html
-
 const expressModule = require('express')
+const app = expressModule()
+const fsModule = require('fs')
 const PORT = process.env.PORT || 3000
 
-expressModule()
-  .get('/', (req, res) => res.send('Hello World!'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+let pagesMapTuples = [
+  { remotePath: '/text', localPath: './views/pages/index.html' },
+]
+
+let rootPage = 'Hello World!'
+
+pagesMapTuples.forEach(function(value, offset){
+  app.get(value.remotePath, (req, res) => res.send(fsModule.readFileSync(value.localPath, {encoding: 'utf8'})))
+  rootPage += `<div><a href="${ value.remotePath }">${ value.remotePath }</a></div>`
+});
+
+app.get('/', (req, res) => res.send(rootPage));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
