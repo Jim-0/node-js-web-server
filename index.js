a06e41778eac9cf79f9947c3dd5b93346ab83227
Jim <jim~@126.com>
2022-03-18 14:56:42 +0800

    add: delegate method to get resource

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 
 const expressModule = require('express')
 const app = expressModule()
+
+const httpsModule = require('https');
+const httpsProxyAgentModule = require('https-proxy-agent');
+
 const fsModule = require('fs')
 const PORT = process.env.PORT || 3000
+const HTTPS_PROXY = process.env.HTTPS_PROXY
+const GET_SECRET = process.env.GET_SECRET || 'url'
 
-let pagesMapTuples = [
+function getResource(onURL, callBack) {
+  let options = {}
+  if (null != HTTPS_PROXY) {
+    console.log('HTTPS_PROXY:', HTTPS_PROXY)
+    let agent = new httpsProxyAgentModule(HTTPS_PROXY);
+    options.agent = agent;
+  }
+  httpsModule.get(onURL, options, (res) => { callBack(res) });
+}
+
+// ???: Is it possible to result in a dead cycle?
+app.get('/get', (req, res) => {
+  const prefix = `/get?q=${ GET_SECRET }:`
+  if (!req.url.startsWith(prefix)) { return }
+  let trustedURL = req.url.substring(prefix.length)
+  getResource(trustedURL, (response) => { response.pipe(res) })
+});
+
+let pagesMap = [
   { remotePath: '/text', localPath: './views/pages/index.html' },
 ]
 
 let rootPage = 'Hello World!'
 
-pagesMapTuples.forEach(function(value, offset){
+pagesMap.forEach(function(value, offset){
   app.get(value.remotePath, (req, res) => res.send(fsModule.readFileSync(value.localPath, {encoding: 'utf8'})))
   rootPage += `<div><a href="${ value.remotePath }">${ value.remotePath }</a></div>`
 });
